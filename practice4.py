30a196ddd0c31dffb66927ce83676229e3cbf86a
pratikrandive04 <al2018cis@gmail.com>
2024-09-12 19:46:20 +0530

    Displayed the first and last colors from the given list

---
+++ practice4.py
@@ -0,0 +1,7 @@
+# Write a Python program to display the first and last colors from the following list.
+
+color_list = ["Red","Green","White" ,"Black"]
+
+length = len(color_list)
+print("First : ", color_list[0])
+print("Last : ", color_list[length-1])
